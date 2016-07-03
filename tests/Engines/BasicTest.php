df0f8937e39ad4f31ca54a1d96232337d9adb8bb
bublak <pavel.filipcik@gmail.com>
2016-07-03 20:56:06 +0200

    conflict merge

--- tests/Engines/BasicTest.php
+++ tests/Engines/BasicTest.php
@@ -1,24 +1,16 @@
 <?php
 
 use bublak\phpunitmultirunner\Engines\Basic;
-<<<<<<< HEAD
 use bublak\phpunitmultirunner\Engines\Executors\PrinterForTest;
-=======
->>>>>>> 8ef53db528c5df748347a93479d596a7c5eca651
 
 class BasicTest extends PHPUnit_Framework_TestCase {
 
     public function testA() {
-<<<<<<< HEAD
         $basicEngine = new Basic(new PrinterForTest());
-=======
-        $basicEngine = new Basic();
->>>>>>> 8ef53db528c5df748347a93479d596a7c5eca651
         $basicEngine->runUnits(array());
 
         self::assertTrue(True);
     }
-<<<<<<< HEAD
 
     public function testBB() {
         $testNameA = 'qbc.test';
@@ -30,6 +22,4 @@ class BasicTest extends PHPUnit_Framework_TestCase {
         self::assertTrue(isset($results[0]['execTime']));
         self::assertEquals(array('OK', 'phpunit ' . $testNameA), $results[0]['result']);
     }
-=======
->>>>>>> 8ef53db528c5df748347a93479d596a7c5eca651
 }
